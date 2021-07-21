a2f26f8727dff9c09a43d31ae8f788835b96d72d
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-07-21 19:24:37 -0300

    Update capture_syscall.rb

--- test/support/capture_syscall.rb
+++ test/support/capture_syscall.rb
@@ -6,10 +6,10 @@ module CaptureSyscall
   def capture_syscall
     exit_status = nil
 
-    stdout, stderr = Timeout.timeout(5) do
+    stdout, stderr = Timeout.timeout(1) do
       capture_subprocess_io do
         yield
-        exit_status = $?.exitstatus
+        exit_status = $CHILD_STATUS.exitstatus
       end
     end
 
