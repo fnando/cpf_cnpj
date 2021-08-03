1ee12b808465343db3d896508b5b44dff2e72095
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-08-02 22:34:23 -0300

    Update capture_syscall.rb

--- test/support/capture_syscall.rb
+++ test/support/capture_syscall.rb
@@ -5,8 +5,8 @@ require "timeout"
 module CaptureSyscall
   def capture_syscall(cmd)
     stdout = stderr = `#{cmd} 2>&1`
-    exit_status = $?.success? ? 0 : 1
-    puts "AAAAAAAA: |#{stdout}| --- |#{stderr}| --- |#{exit_status}|"
+    exit_status = $?.exitstatus
+    puts "BBB: |#{stdout}| --- |#{stderr}| --- |#{exit_status}|"
     [exit_status, stdout, stderr]    
   end
 end
