11b896c39fd478ece4585ebf4e69a0091a7c884e
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-08-02 22:31:46 -0300

    Update capture_syscall.rb

--- test/support/capture_syscall.rb
+++ test/support/capture_syscall.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 require "timeout"
-require "open3"
 
 module CaptureSyscall
   def capture_syscall(cmd)
-    exit_status = nil
-    
-    stdout, stderr = Timeout.timeout(1) do
-      capture_subprocess_io do
-        exit_status = system(cmd) ? 0 : 1
-      end
-    end
-    
-    [exit_status, stdout, stderr]
-
-    #Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-    #  [wait_thr.value.exitstatus, stdout.read, stderr.read]
-    #end    
+    stdout = stderr = `#{cmd} 2>&1`
+    exit_status = $?.success? ? 0 : 1
+    [exit_status, stdout, stderr]    
   end
 end
