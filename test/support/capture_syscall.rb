7af532b4636c74b98e9ef3a62f22789cbaaf9299
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-08-02 22:32:51 -0300

    Update capture_syscall.rb

--- test/support/capture_syscall.rb
+++ test/support/capture_syscall.rb
@@ -6,6 +6,7 @@ module CaptureSyscall
   def capture_syscall(cmd)
     stdout = stderr = `#{cmd} 2>&1`
     exit_status = $?.success? ? 0 : 1
+    puts "AAAAAAAA: |#{stdout}| --- |#{stderr}| --- |#{exit_status}|"
     [exit_status, stdout, stderr]    
   end
 end
