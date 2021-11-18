a4be61be846d23bc124a2903d2da08fe0f1ee73d
Yan Donadi <yan.donadi@gmail.com>
2021-11-18 00:40:25 -0300

    Added format as a class method

--- lib/cpf.rb
+++ lib/cpf.rb
@@ -36,6 +36,10 @@ class CPF
     formatted ? cpf.formatted : cpf.stripped
   end
 
+  def self.format(number, strict: false)
+    new(number, strict).formatted
+  end
+
   def initialize(number, strict = false)
     @number = number.to_s
     @strict = strict
